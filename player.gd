6e9bd3bdfc07a3eeff5d97ec7fb2c327c7bca57c
Wolfang Torres <wolfang.torres@gmail.com>
2025-04-01 22:33:23 -0400

    add jump animation

--- player.gd
+++ player.gd
@@ -5,9 +5,7 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 # rotation speed in rads/s
-@export var rotation_speed = PI*1.5
-# rotation speed for pitch in rads/s
-@export var pitch_speed = PI*2
+@export var rotation_speed = PI * 2
 # Vertical impulse applied to the character upon jumping in meters per second.
 @export var jump_impulse = 20
 # Vertical impulse applied to the character upon bouncing over a mob in
@@ -17,11 +15,11 @@ extends CharacterBody3D
 var target_velocity = Vector3.ZERO
 var current_direction = Vector3.FORWARD
 var current_pitch = 0
+var last_y_velocity = 0
 
 func _physics_process(delta):
     # We create a local variable to store the input direction.
     var direction = Vector3.ZERO
-
     # We check for each move input and update the direction accordingly.
     if Input.is_action_pressed("move_right"):
         direction.x += 1
@@ -33,33 +31,30 @@ func _physics_process(delta):
         direction.z += 1
     if Input.is_action_pressed("move_forward"):
         direction.z -= 1
-
-    if direction != Vector3.ZERO:
+    if direction != Vector3.ZERO or target_velocity.y != last_y_velocity:
+        # Get XZ rotation 
         direction = direction.normalized()
-        if direction != current_direction:
+        if direction != Vector3.ZERO and direction != current_direction:
             var angle = current_direction.signed_angle_to(direction, Vector3.UP)
-            var current_rotation = angle if abs(angle) <= rotation_speed*delta else rotation_speed*delta*sign(angle)
+            var current_rotation = angle if abs(angle) <= rotation_speed * delta else rotation_speed * delta * sign(angle)
             current_direction = current_direction.rotated(Vector3.UP, current_rotation)
-
+        if target_velocity.y != last_y_velocity:
+            current_pitch = atan(target_velocity.y / jump_impulse)
+            last_y_velocity = position.y
         # Setting the basis property will affect the rotation of the node.
-        $Pivot.basis = Basis.looking_at(current_direction)
-    
+        $Pivot.basis = Basis.looking_at(current_direction.rotated(current_direction.cross(Vector3.UP), current_pitch))
     # Ground Velocity
     target_velocity.x = direction.x * speed
     target_velocity.z = direction.z * speed
-
     # Vertical Velocity
     if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-        target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-    
+        target_velocity.y -= (fall_acceleration * delta)
+    else:
+        target_velocity.y = 0
     # Jumping
     if is_on_floor() and Input.is_action_just_pressed("jump"):
         target_velocity.y = jump_impulse
-    
-    # Animation 
-    if is_on_floor():
-
-    
+    # Check Collisions
     for index in range(get_slide_collision_count()):
         var collision = get_slide_collision(index)
         if collision.get_collider() == null:
@@ -70,8 +65,6 @@ func _physics_process(delta):
                 mob.squash(bounce_impulse)
                 target_velocity.y = bounce_impulse
                 break
-
-
     # Moving the Character
     velocity = target_velocity
     move_and_slide()
